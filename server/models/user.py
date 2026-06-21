ea5586fce53a381c59bc8b463a8df1dce3b35a89
moondev010 <rekdev07@proton.me>
2026-06-20 21:07:59 -0600

    Add full CRUD operations to the User model

--- server/models/user.py
+++ server/models/user.py
@@ -7,3 +7,35 @@ class User:
             "INSERT INTO users(email, password, username) VALUES(?, ?, ?)",
             (email, password, username)
         )
+
+    def get(self, email):
+        self.database.execute_query(
+            "SELECT * FROM users WHERE email = ?",
+            (email,)
+        )
+
+    def update(self, email, params):
+
+        set_chunk = ""
+
+        for i, (key, value) in enumerate(params.items()):
+            set_chunk += f"{key}=?"
+
+            if i < len(params) - 1:
+                set_chunk += ", "
+
+        statement = f"UPDATE users SET {set_chunk} WHERE email = ? LIMIT 1"
+
+        final_params = list(params.values())
+        final_params.append(email)
+
+        self.database.execute_query(
+            statement,
+            tuple(final_params)
+        )
+
+    def delete(self, email):
+        self.database.execute_query(
+            "DELETE FROM users WHERE email = ?",
+            (email,)
+        )
